1ac32568aef57b039e0d1d4ae6fea2c5f98dd32e
Timits1 <will4f21@yahoo.com>
2019-07-17 01:41:36 -0700

    fix random color generator ttt text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,22 @@
-// Define some colours
-var colours = [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "purple"    
-];
 
-// Retrieve the words
-var text = $(".rc").html().split(" ");
-
-// Empty the elment
-$(".rc").empty();
-
-// Iterate over the words
-$.each(text, function(i, word) {
-    if(i != text.length) {
-        word += " "; // Add space after word 
-    }
-
-     // Get random color
-     setTimeout(function() {
-      $(colourIndex).reset();
-      
-    }, 500);
-     var colourIndex = Math.floor(Math.random() * colours.length);
-
-    $("<span>")
-        .html(word)
-        .css("color", colours[colourIndex])
-        .appendTo($(".rc"));
-
-       
-});
-
-
-// setTimeout(function() {
-//   $(text).reset();
-// }, 1000);
-
-
-
-// add jQuery text for All Done!
-// create variables for correctAnswer, wrongAnswer and unAnswer set to zero
+// create variables for correctAnswer, wrongAnswer and unAnswered set to zero
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var unAnswered = 0;
+
+var colors = ["blue", "white", "red"]
+var currentColor = 0
+var lis = document.querySelectorAll(".rc")
+
+function changeColor() {
+  --currentColor
+  if (currentColor < 0) currentColor = colors.length -1
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].style.color = colors[(currentColor +i) % colors.length]
+  }
+}
+
+setInterval(changeColor, 1000);
 
 // set timer --- wrap timer in on-click function  
 $("#hideDiv").click( function(){
@@ -57,14 +27,10 @@ var downloadTimer = setInterval(function(){
   if(timeleft <= 0){
     clearInterval(downloadTimer);
     document.getElementById("timer").innerHTML = "Finished!";
-    
     // line 38 var ifUnAnswered
     clearInterval(ifUnAnswered());
-    
-
-  }
-
-}, 1000); 
+    }
+  }, 1000); 
 
 });
 
@@ -79,56 +45,33 @@ $("#hideDiv").click( function(){
 });
 
  // ifUnAnswered
- 
-var ifUnAnswered = function checkFunc() {
+ var ifUnAnswered = function checkFunc() {
 
     if (!$(".answer:checked").length < 3 ) {
         unAnswered = 3 - $(".answer:checked").length;
         $("#unAnswered").text(unAnswered);
-        //alert("Not checked")
-    } else {
-        //alert("Checked");
-    }
-    
-} // ifUnAnswered
-
-////
-// for loop to check if radio is unchecked
-
-
-// for (var i=0; i<input.answer.length; i++)  {
-// if (!$("input[name='answer']:checked"))  {
-
-//     unAnswered = "input[name='answer'][i]";
-//     unAnswered++;
-
-// }
-// }
-/////
+    } 
+} 
 
 // Hide results until game over
 $(document).ready(function() {
     setTimeout(function(){
     $('#thisDiv').fadeIn();
-    }, 10000);
+    }, 14000);
     });
 
-
+// Increment wins and losses
 $(document).ready(function(){
     $('.answer').change(function(){
       if(this.value === "correct") {
           correctAnswer++;
         $("#correctAnswer").text(correctAnswer);
-        alert('You have selected the correct answer!');
       } else if(this.value === "wrong"){
         wrongAnswer++;
         $("#wrongAnswer").text(wrongAnswer);
-        alert('You have selected the wrong answer!');
-
       } 
     });
-    
-  });
+ });
 
 
 
