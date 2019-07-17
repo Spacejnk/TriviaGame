576b482603fc07b7a7e1f6289d2f2ec639a7666b
Timits1 <will4f21@yahoo.com>
2019-07-16 23:20:06 -0700

    fix start and radio button onclick function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,47 @@
+// Define some colours
+var colours = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple"    
+];
+
+// Retrieve the words
+var text = $(".rc").html().split(" ");
+
+// Empty the elment
+$(".rc").empty();
+
+// Iterate over the words
+$.each(text, function(i, word) {
+    if(i != text.length) {
+        word += " "; // Add space after word 
+    }
+
+     // Get random color
+     setTimeout(function() {
+      $(colourIndex).reset();
+      
+    }, 500);
+     var colourIndex = Math.floor(Math.random() * colours.length);
+
+    $("<span>")
+        .html(word)
+        .css("color", colours[colourIndex])
+        .appendTo($(".rc"));
+
+       
+});
+
+
+// setTimeout(function() {
+//   $(text).reset();
+// }, 1000);
+
+
+
 // add jQuery text for All Done!
 // create variables for correctAnswer, wrongAnswer and unAnswer set to zero
 var correctAnswer = 0;
@@ -38,16 +82,29 @@ $("#hideDiv").click( function(){
  
 var ifUnAnswered = function checkFunc() {
 
-    if (!$("input[name='answer']:checked").length < 3 ) {
-        unAnswered++;
+    if (!$(".answer:checked").length < 3 ) {
+        unAnswered = 3 - $(".answer:checked").length;
         $("#unAnswered").text(unAnswered);
-        alert("Not checked")
+        //alert("Not checked")
     } else {
-        alert("Checked");
+        //alert("Checked");
     }
     
 } // ifUnAnswered
 
+////
+// for loop to check if radio is unchecked
+
+
+// for (var i=0; i<input.answer.length; i++)  {
+// if (!$("input[name='answer']:checked"))  {
+
+//     unAnswered = "input[name='answer'][i]";
+//     unAnswered++;
+
+// }
+// }
+/////
 
 // Hide results until game over
 $(document).ready(function() {
@@ -58,12 +115,12 @@ $(document).ready(function() {
 
 
 $(document).ready(function(){
-    $('input[name=answer]').change(function(){
-      if($('input[name=answer]:checked').val() === "correct") {
+    $('.answer').change(function(){
+      if(this.value === "correct") {
           correctAnswer++;
         $("#correctAnswer").text(correctAnswer);
         alert('You have selected the correct answer!');
-      } else if($('input[name=answer]:checked').val() === "wrong"){
+      } else if(this.value === "wrong"){
         wrongAnswer++;
         $("#wrongAnswer").text(wrongAnswer);
         alert('You have selected the wrong answer!');
